5a32676e419b499b0f70f60c14199fd8dddb3bf5
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-07-12 00:15:02 -0300

    Se crean los componentes y se dan los estilos iniciales de la app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { TodoCounter } from "./components/TodoCounter/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch/TodoSearch";
+import { TodoList } from "./components/TodoList/TodoList";
+import { TodoItem } from "./components/TodoItem/TodoItem";
+import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton";
+
+
+// import './App.css';
+
+const todos = [
+  { text: 'cortar cebolla', completed: false },
+  { text: 'hacer el curso de introducción a React', completed: false },
+  { text: 'llorar con la llorona', completed: false }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
