c79eee174c7f9bc506ac98d8048c46cd9269102a
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-07-13 23:38:17 -0300

    Se agrega el metodo de filtrado de tareas desde el input

--- src/App.js
+++ src/App.js
@@ -9,20 +9,43 @@ import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton
 
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'cortar cebolla', completed: true },
   { text: 'hacer el curso de introducción a React', completed: false },
   { text: 'llorar con la llorona', completed: false }
 ]
 
 function App() {
+  const [ todos, setTodos ] = React.useState(defaultTodos);
+  const [ searchValue, setSearchValue ] = React.useState('');
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter( todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    })
+  }
+
+
 
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
